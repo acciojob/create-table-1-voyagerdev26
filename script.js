02aba913b22974386a786c5ebae1c00e3711b95b
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-02-16 18:56:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
 function insert_Row() {
     //Write your code here
+	let table=document.getElementById("sampleTable");
+	let tr=document.createElement("tr");
+	let td1=document.createElement("td");
+	let td2=document.createElement("td");
+	td1.innerText="New Cell1";
+	td2.innerText="New Cell2";
+	tr.append(td1,td2);
+	table.prepend(tr);
   
   
 }
